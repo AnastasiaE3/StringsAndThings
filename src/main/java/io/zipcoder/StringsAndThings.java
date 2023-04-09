ece150e72b5dae94499d516aa030ffd6a325f956
AnastasiaE3 <125825396+AnastasiaE3@users.noreply.github.com>
2023-04-09 01:44:38 -0400

    finished

--- src/main/java/io/zipcoder/StringsAndThings.java
+++ src/main/java/io/zipcoder/StringsAndThings.java
@@ -1,6 +1,8 @@
 package io.zipcoder;
 
 
+import org.apache.maven.shared.utils.StringUtils;
+
 /**
  * @author tariq
  */
@@ -15,7 +17,14 @@ public class StringsAndThings {
      *           countYZ("day fyyyz"); // Should return 2
      */
     public Integer countYZ(String input){
-        return null;
+        int count = 0;
+        input = input.toLowerCase() + " ";
+        for (int i = 0; i < input.length() - 1; i++) {
+            if ((input.charAt(i) == 'y' || input.charAt(i) == 'z') && !Character.isLetter(input.charAt(i + 1))) {
+                count++;
+            }
+        }
+        return count;
     }
 
     /**
@@ -28,7 +37,24 @@ public class StringsAndThings {
      *           removeString("Hello there", "x") // Should return "Hello there"
      */
     public String removeString(String base, String remove){
-        return null;
+        String baseLowerCase = base.toLowerCase(); // convert base string to lowercase
+        String removeLowerCase = remove.toLowerCase(); // convert remove string to lowercase
+        int removeLength = removeLowerCase.length(); // get the length of remove string
+        StringBuilder result = new StringBuilder(); // initialize a StringBuilder to store the result
+        int startIndex = 0; // initialize the start index for searching remove string
+
+        while (true) {
+            int index = baseLowerCase.indexOf(removeLowerCase, startIndex); // search for the remove string in the base string
+            if (index == -1) { // if remove string is not found, append the remaining base string to the result and break the loop
+                result.append(base.substring(startIndex));
+                break;
+            } else { // if remove string is found, append the base string before the remove string to the result and update the start index
+                result.append(base.substring(startIndex, index));
+                startIndex = index + removeLength;
+            }
+        }
+
+        return result.toString();
     }
 
     /**
@@ -39,10 +65,15 @@ public class StringsAndThings {
      *           containsEqualNumberOfIsAndNot("This is notnot") // Should return true
      *           containsEqualNumberOfIsAndNot("noisxxnotyynotxisi") // Should return true
      */
-    public Boolean containsEqualNumberOfIsAndNot(String input){
-        return null;
+    public Boolean containsEqualNumberOfIsAndNot(String input) {
+        int c = StringUtils.countMatches(input, "not");
+        int c1 = StringUtils.countMatches(input, "is");
+        if (c == c1) {
+            return true;
+        } else {
+            return false;
+        }
     }
-
     /**
      * We'll say that a lowercase 'g' in a string is "happy" if there is another 'g' immediately to its left or right.
      * Return true if all the g's in the given string are happy.
@@ -51,7 +82,15 @@ public class StringsAndThings {
      *           gHappy("xxggyygxx") // Should return  false
      */
     public Boolean gIsHappy(String input){
-        return null;
+        if (input.length() < 2) { // check if input length is less than 2
+            return true;
+        }
+        for (int i = 1; i < input.length() - 1; i++) { // iterate over each character in the input string
+            if (input.charAt(i) == 'g' && input.charAt(i - 1) != 'g' && input.charAt(i + 1) != 'g') { // check if the current g is not happy
+                return false;
+            }
+        }
+        return true;
     }
 
 
@@ -63,6 +102,16 @@ public class StringsAndThings {
      *            countTriple("a") // Should return 0
      */
     public Integer countTriple(String input){
-        return null;
+        int count = 0;
+        if (input.length() < 3) { // check if input length is less than 3
+            return 0;
+        }
+        for (int i = 0; i < input.length() - 2; i++) { // iterate over each character in the input string
+            char currentChar = input.charAt(i);
+            if (currentChar == input.charAt(i + 1) && currentChar == input.charAt(i + 2)) { // check if the current char is repeated three times in a row
+                count++;
+            }
+        }
+        return count;
     }
 }
